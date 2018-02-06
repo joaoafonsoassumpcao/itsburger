8fa108ab216f401ec8a3f805edfc14556ca6e717
joao <joaoafonsoassumpcao@gmail.com>
2018-02-06 16:05:35 -0200

    primeiro commit

---
+++ public/tweetie/api/config.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'MRdfddcwD9FBP2bp3EwagoAkb');
+    define('CONSUMER_SECRET', 'RR5bBFsSSwJs5kIQeseOe2iSelFNk7SNv24AhEl1f0lCllgoAZ');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '834354680985767936-S0f0kyly1C69Wz0gKMOgTnxu2Sdy0pZ');
+    define('ACCESS_SECRET', 's3eBAkV7p3Ta4Ekxdkoz3GSWyAVM4d14Rn0xhQmd4QFPF');
+	
+	// Cache Settings
+	define('CACHE_ENABLED', true);
+	define('CACHE_LIFETIME', 3600); // in seconds
+	define('HASH_SALT', md5(dirname(__FILE__)));
